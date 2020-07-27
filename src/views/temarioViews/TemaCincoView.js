2b66aba48d0d16f3047e5f5b979eb4b8f26be4f7
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-27 17:26:55 -0500

    Update TemaCincoView.js

--- src/views/temarioViews/TemaCincoView.js
+++ src/views/temarioViews/TemaCincoView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { CardBody, CardHeader, Col, Row } from 'reactstrap'
-import cinco from '../../assets/img/ImgEmpatia/cinco.jpg'
-
+import cinco01 from '../../assets/img/ImgErgonometria/cinco01.png'
+import cinco02 from '../../assets/img/ImgErgonometria/cinco02.png'
+import objetivo from '../../assets/img/ImgErgonometria/objetivo.jpg'
 
 class TemaCincoView extends Component {
   loading = () => (
@@ -12,48 +13,61 @@ class TemaCincoView extends Component {
     return (
       <div className="animated fadeIn ">
         <CardHeader className="text-left">
-          <h3>El Ladrón de Problemas</h3>
+          <h3>Ergonometría Digítal</h3>
         </CardHeader>
         <CardBody className="">
-          <Row>
+          <Row className="centrado-fila">
             <Col xs="12">
               <h2 className="animated fadeInDown delay-0s">
-                <b>Voy Ayudar</b>
+                <b>Limpia tus dispositivos</b>
               </h2>
+            </Col>
+          </Row>
+          <Row className="centrado-fila">
+            <Col xs="7">
               <p className="animated fadeInDown delay-1s">
-                Ya son pocos los problemas que te quedan ¿verdad? , pues te voy
-                ayudar a que sean menos. De los problemas que identificaste
-                retira todos aquellos que no tienen una solución, ya que no te
-                es posible intervenir y únicamente toma en tus manos los que se
-                pueden solucionar.
-              </p>
-              <p>
-                Ya son pocos los problemas que te quedan ¿verdad? , pues te voy
-                ayudar a que sean menos. De los problemas que identificaste
-                retira todos aquellos que <b>no tienen una solución</b>, ya que
-                no te es posible intervenir y únicamente toma en tus manos los
-                que <b>se pueden solucionar</b>.{' '}
-              </p>
-              <p>
-                <b>Pues claro ¡el ladrón de problemas!</b>
-              </p>
-              <p>
-                <b>
-                  A veces lo que te ayuda a solucionar un problema, es una
-                  actitud.{' '}
-                </b>
-              </p>
-              <p>
-                Y si necesitas saber como solucionar un problema, acompáñame el
-                el siguiente curso de SOLUCIÓN DE PROBLEMAS.{' '}
+                Realiza una rutina diara de limpieza y desinfeccion antes de
+                usar tus dispositivos y perifericos.
               </p>
             </Col>
-            <Col xs="12" className="centrado-fila  animated fadeInDown">
+            <Col xs="4" className="centrado-fila  animated fadeInDown">
               <img
-                src={cinco}
+                src={cinco01}
                 style={{ width: 350 }}
-                alt="escucha-activa.jpg"
-                className="img-fluid   animated fadeInUpBig delay-1s  "
+                alt="cinco01"
+                className="img-fluid bordeImagen  animated fadeInUpBig delay-1s  "
+              />
+            </Col>
+          </Row>
+          <Row className="centrado-fila">
+            <Col xs="4" className="centrado-fila  animated fadeInDown">
+              <img
+                src={cinco02}
+                style={{ width: 350 }}
+                alt="cinco02"
+                className="img-fluid  bordeImagen animated fadeInUpBig delay-1s  "
+              />
+            </Col>
+            <Col xs="7">
+              <p className="animated fadeInDown delay-1s">
+                Ya que son portadores de una gran cantidad de microorganismos
+                que podrian afectar tu salud.
+              </p>
+            </Col>
+          </Row>
+          <Row className="centrado-fila">
+            <Col xs="7">
+              <p className="animated fadeInDown delay-1s">
+                Higieniza con mayor cuidado los que estan en contacto con otras
+                personas.
+              </p>
+            </Col>
+            <Col xs="4" className="centrado-fila  animated fadeInDown">
+              <img
+                src={objetivo}
+                style={{ width: 350 }}
+                alt="objetivo"
+                className="img-fluid  bordeImagen animated fadeInUpBig delay-1s  "
               />
             </Col>
           </Row>
