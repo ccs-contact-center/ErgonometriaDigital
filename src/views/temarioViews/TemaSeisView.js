3f0bdbf6f9626c70e02c893a846eb80ad0e50185
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-27 17:27:01 -0500

    Update TemaSeisView.js

--- src/views/temarioViews/TemaSeisView.js
+++ src/views/temarioViews/TemaSeisView.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { CardBody, Col, Row, CardHeader } from 'reactstrap'
-import ReactPlayer from 'react-player'
-import conclusion from '../../assets/img/ImgEmpatia/conclusion.png'
+import seis from '../../assets/img/ImgErgonometria/seis.jpg'
 
 class TemaSeisView extends Component {
   loading = () => (
@@ -12,48 +11,18 @@ class TemaSeisView extends Component {
     return (
       <div className="animated fadeIn ">
         <CardHeader className="text-left">
-          <h3>Empatía y Cortesía</h3>
+          <h3>Ergonometría Digítal</h3>
         </CardHeader>
         <CardBody className="">
           <Row className="">
             <Col xs="12" className="animated zoomInUp delay-1s">
-              <h2>¿Hasta donde puede llegar tu empatía?</h2>
+              <h2>Dormir</h2>
             </Col>
-            <Col md="12" className="centrado-fila animated zoomInUp delay-2s">
-              <div className=" centrado-fila">
-                <ReactPlayer
-                  url="https://www.youtube.com/watch?v=nfYPktsd9bs"
-                  controls={true}
-                />
-              </div>
-            </Col>
-            <Col md="12" className="mt-4 animated zoomInUp delay-3s">
-              <p>
-                Podemos concluir indicando que la empatía es una reacción física
-                de tu cuerpo pero también es una actitud.
-              </p>
-              <p style={{ marginBottom: '0px' }}>
-                <b>Conclusiones:</b>
-              </p>
-              <ul>
-                <li>Todos somos empáticos, lo traemos en el organismo.</li>
-                <li>
-                  {' '}
-                  Unos somos más empáticos que otros, pero podemos trabajar en
-                  ello.
-                </li>
-                <li>La cortesia es una acción y es una actitud.</li>
-                <li>
-                  {' '}
-                  La empatía y cortesía son necesarios no solo para mi trabajo,
-                  sino para la vida misma.
-                </li>
-                <li></li>
-              </ul>
 
-              <p>
-                Espero te haya gustado el curso pero sobre todo hayas aprendido
-                algo nuevo y lo apliques en todos los ambitos de tu vida.
+            <Col md="12" className="mt-4 animated zoomInUp delay-2s">
+              <p className="text-center">
+                Por ultimo recuerda dejar de usar tu móvil u ordenador por lo
+                menos 30 minutos antes de dormir, asi mejoraras tu descanso.
               </p>
             </Col>
             <Col
@@ -61,10 +30,10 @@ class TemaSeisView extends Component {
               className="mt-4 animated zoomInUp delay-4s centrado-fila"
             >
               <img
-                src={conclusion}
+                src={seis}
                 style={{ width: 360 }}
-                alt="conclusion"
-                className="img-fluid  animated  fadeInDown  delay-1s"
+                alt="seis"
+                className="img-fluid bordeImagen  animated  fadeInDown  delay-3s"
               />
             </Col>
           </Row>
