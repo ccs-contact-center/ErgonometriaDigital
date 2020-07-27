86afb983e025a0d97f80d7d1e15d7d9fe7947d19
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-27 17:31:57 -0500

    Update API_CCS.js

--- src/services/API_CCS.js
+++ src/services/API_CCS.js
@@ -1,39 +1,48 @@
-import AuthService from "./AuthService";
+import AuthService from './AuthService'
 
-const hostURL = "https://api.ccscontactcenter.com";
+const hostURL = 'https://api.ccscontactcenter.com'
 //const hostURL = 'http://localhost:3020'
 
 export default class API_CCS {
   constructor() {
-    this.Auth = new AuthService();
-    this.fetch = this.fetch.bind(this);
+    this.Auth = new AuthService()
+    this.fetch = this.fetch.bind(this)
   }
 
   async fetch(url, options) {
     const headers = {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    };
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
 
     if (await this.Auth.loggedIn()) {
-      headers["Authorization"] = "Bearer " + (await this.Auth.getToken());
+      headers['Authorization'] = 'Bearer ' + (await this.Auth.getToken())
     } else {
-      window.location.href = "/login";
+      window.location.href = '/login'
     }
 
     return fetch(url, {
       headers,
-      ...options
+      ...options,
     })
       .then(this._checkStatus)
-      .then(response => response.json());
+      .then((response) => response.json())
   }
 
   getCampaignAvatar(id) {
-    return this.fetch(hostURL + "/v1/Campaigns/Avatar?id=" + id, {
-      method: "GET"
-    }).then(res => {
-      return Promise.resolve(res);
-    });
+    return this.fetch(hostURL + '/v1/Campaigns/Avatar?id=' + id, {
+      method: 'GET',
+    }).then((res) => {
+      return Promise.resolve(res)
+    })
+  }
+
+  guardaActividad(data) {
+    return this.fetch(hostURL + '/v1/capacitacion/Actividades', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((res) => {
+      return Promise.resolve(res)
+    })
   }
 }
