bf07f11720b6304c6f4ec60bfbe7c8c063004a4e
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-08-11 18:01:55 -0500

    Update Actividad1View.js

--- src/views/temarioViews/Actividad1View.js
+++ src/views/temarioViews/Actividad1View.js
@@ -10,6 +10,7 @@ import {
   Label,
   Input,
 } from 'reactstrap'
+import swal from "sweetalert"
 import AuthService from '../../services/AuthService'
 import API_CCS from '../../services/API_CCS'
 const API = new API_CCS()
@@ -46,10 +47,37 @@ class Actividad1View extends Component {
 
   async onSave(e) {
     try {
-      var respuesta = await API.guardaActividad(this.state)
-      alert('Se guardo la actividad: 1, con id: ' + respuesta[0].id)
+      var respuesta = await API.guardaActividad(this.state);
+
+      swal({
+        title: "Status Actividad",
+        text: "Se guardo la actividad: 1, con id: " + respuesta[0].id,
+        icon: "success",
+        dangerMode: true,
+        button: {
+          text: "Aceptar",
+          value: true,
+          visible: true,
+          className: "btn btn-primary",
+          reset: true,
+        },
+      });
+      
+      // alert("Se guardo la actividad: 1, con id: " + respuesta[0].id);
     } catch (err) {
-      console.log('loggea si hay un error')
+      swal({
+        title: "Status Actividad",
+        text: "No se guardo la actividad: 1, Intenta de nuevo. ",
+        icon: "error",
+        dangerMode: true,
+        button: {
+          text: "Cerrar",
+          value: true,
+          visible: true,
+          className: "btn btn-primary ",
+        },
+      });
+      console.log("loggea si hay un error");
     }
   }
 
